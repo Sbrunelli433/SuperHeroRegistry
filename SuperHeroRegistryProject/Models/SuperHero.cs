d064106861157bc2226ce5574220ed6a20503521
Sbrunelli433 <brunes46@gmail.com>
2019-06-21 10:39:57 -0500

    created SuperHero Database with table including int Id, string Name, string AlterEgoName, string PrimaryAbility, string SecondaryAbility, string Catchphrase

---
+++ SuperHeroRegistryProject/Models/SuperHero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroRegistryProject.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+
+        public string  Name { get; set; }
+        public string AlterEgoName { get; set; }
+        public string PrimaryAblity{ get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase{ get; set; }
+
+    }
+}
