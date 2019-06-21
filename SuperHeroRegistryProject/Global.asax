ce0ea2de8b98f4d2249b5550bbd405476a451c87
Sbrunelli433 <brunes46@gmail.com>
2019-06-21 10:31:17 -0500

    init

---
+++ SuperHeroRegistryProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperHeroRegistryProject.MvcApplication" Language="C#" %>
---
+++ SuperHeroRegistryProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperHeroRegistryProject.Startup))]
+namespace SuperHeroRegistryProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
